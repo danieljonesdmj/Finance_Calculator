5892db98c8eb660e597c85a3ca45f50698aa1cf6
danieljonesdmj <danieljonesdmj@gmail.com>
2019-03-22 23:15:09 +0000

    Add test for getting first variable

--- lib/input.rb
+++ lib/input.rb
@@ -1,6 +1,10 @@
 class Input
+
+    attr_reader :first_variable
    
     def get_first_variable
+        puts "What is the first variable?"
+        @first_variable = gets.chomp
     end
 
 end
--- spec/input_spec.rb
+++ spec/input_spec.rb
@@ -1,5 +1,13 @@
 require 'input'
 
 describe Input do
-    it { is_expected.to respond_to :get_first_variable }    
+    it { is_expected.to respond_to :get_first_variable }
+    
+    describe '.get_first_variable' do
+        it 'gets first variables from user' do
+            input = Input.new
+            input.get_first_variable
+            expect(input.first_variable).to eq('t')
+        end
+    end
 end
