07ec40be52780b1117157204fa74bbb0a9aa3732
danieljonesdmj <danieljonesdmj@gmail.com>
2019-03-22 23:31:12 +0000

    check whether user input is valid

--- lib/input.rb
+++ lib/input.rb
@@ -3,8 +3,16 @@ class Input
     attr_reader :first_variable
    
     def get_first_variable
+        valid_inputs = ['i', 'p', 'r', 't']    
+        variables = {}
+        
         puts "What is the first variable?"
         @first_variable = gets.chomp
+
+        if valid_inputs.include? first_variable
+            variables[first_variable] = nil
+        else
+        end
     end
 
 end
--- spec/input_spec.rb
+++ spec/input_spec.rb
@@ -1,13 +1,19 @@
 require 'input'
 
 describe Input do
+    let(:input) { Input.new }
     it { is_expected.to respond_to :get_first_variable }
     
     describe '.get_first_variable' do
         it 'gets first variables from user' do
-            input = Input.new
             input.get_first_variable
             expect(input.first_variable).to eq('t')
+        end
+
+        it 'checks user input is valid' do
+            valid_inputs = ['i', 'p', 'r', 't']    
+            input.get_first_variable
+            expect(valid_inputs).to include(input.first_variable)
         end
     end
 end
